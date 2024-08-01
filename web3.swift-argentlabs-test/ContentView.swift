cae791d6ec2661283eccf7e4fed59cf0d9305b38
Jianrong Fan <ffffjr@163.com>
2024-08-01 18:08:29 +0800

    init

---
+++ web3.swift-argentlabs-test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  web3.swift-argentlabs-test
+//
+//  Created by Jianrong Fan on 2024/8/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ web3.swift-argentlabs-test/web3_swift_argentlabs_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  web3_swift_argentlabs_testApp.swift
+//  web3.swift-argentlabs-test
+//
+//  Created by Jianrong Fan on 2024/8/1.
+//
+
+import SwiftUI
+
+@main
+struct web3_swift_argentlabs_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
